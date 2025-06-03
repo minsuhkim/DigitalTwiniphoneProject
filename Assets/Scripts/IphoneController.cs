618fbecf54511809dc19d2209a47a062bcad11a7
minsuhkim <124859537+minsuhkim@users.noreply.github.com>
2025-06-03 17:42:46 +0900

    이동 구현
    가속도 센서 값 보정
    수치 적분 공식 적용

--- Assets/Scripts/IphoneController.cs
+++ Assets/Scripts/IphoneController.cs
@@ -13,9 +13,18 @@ public class IphoneController : MonoBehaviour
     [SerializeField] private Text accelText;
     [SerializeField] private Text velocityText;
 
-    private Vector3 velocity;
-
-
+    private Vector3 currentVelocity = Vector3.zero;
+    private Vector3 prevVelocity = Vector3.zero;
+    private Vector3 prevAcceleration = Vector3.zero;
+    
+    private bool isStarted = false;
+    
+    // // 필터 계수 (0 < alpha < 1), 값이 작을수록 고주파만 통과
+    // [SerializeField] private float accelHighPassAlpha = 0.95f;
+    //
+    // private Vector3 rawAccelFiltered = Vector3.zero;
+    // private Vector3 prevRawAccel = Vector3.zero;
+    
     void Start()
     {
         if (SystemInfo.supportsGyroscope)
@@ -35,25 +44,33 @@ public class IphoneController : MonoBehaviour
             transform.localRotation = gyro;
             gyroQuaternionText.text = "Gyro Quaternion" + Input.gyro.attitude.ToString();
             gyroEulerText.text = "Gyro Euler" + Input.gyro.attitude.eulerAngles.ToString();
+            
+            iphoneRotationText.text = "iphone Quaternion" + transform.localRotation.ToString();
+            iphoneRotationEulerText.text = "iphone Euler" + transform.localRotation.eulerAngles.ToString();
         }
 
-        iphoneRotationText.text = "iphone Quaternion" + transform.localRotation.ToString();
-        iphoneRotationEulerText.text = "iphone Euler" + transform.localRotation.eulerAngles.ToString();
+        if (isStarted)
+        {
+            // 아이폰 좌표계 -> 유니티 좌표계
+            // 로컬 좌표 기준 가속도 -> 월드 좌표 기준 가속도 
+            Vector3 rawAccel = AccelToUnity(Input.acceleration);
+            
+            Vector3 currentAcceleration = gyro * rawAccel;
+            currentAcceleration += Vector3.up;
+            accelText.text = "Accel " + currentAcceleration.ToString();
+        
+            // 속도 = 가속도의 적분
+            currentVelocity = 0.5f * (prevAcceleration +  currentAcceleration) * Time.deltaTime + prevVelocity;
+            velocityText.text = "Velocity " + currentVelocity.ToString();
 
-        // 아이폰 좌표계 -> 유니티 좌표계
-        //Vector3 accel = AccelToUnity(Input.acceleration);
-        Vector3 accel = gyro * AccelToUnity(Input.acceleration);
-        accel += Vector3.up;
-        accelText.text = "Accel " + accel.ToString();
-        //accelText.text = "Accel " + Input.acceleration.ToString();
-
-        // 속도 = 가속도의 적분
-        velocity += accel * Time.deltaTime;
-        velocityText.text = "Velocity " + velocity.ToString();
-
-        // 위치 = 속도의 적분
-        transform.localPosition += velocity * Time.deltaTime;
-        positionText.text = "Position" + transform.localPosition.ToString();
+            // 위치 = 속도의 적분
+            transform.position += 0.5f*(prevVelocity + currentVelocity) * Time.deltaTime;
+            positionText.text = "Position" + transform.localPosition.ToString();
+        
+        
+            prevAcceleration = currentAcceleration;
+            prevVelocity = currentVelocity;
+        }
     }
 
     private Vector3 AccelToUnity(Vector3 original)
@@ -64,5 +81,10 @@ public class IphoneController : MonoBehaviour
     private Quaternion GyroToUnity(Quaternion q)
     {
         return new Quaternion(q.x, q.z, q.y, -q.w);
+    }
+
+    public void OnClickStartButton()
+    {
+        isStarted = true;
     }
 }
